0ff764f8979131e324fc4c699d031b764cff1ce1
Enikeev.00 <maksimenikeev03@mail.ru>
2023-07-05 13:36:52 +0600

    wokr_2 (timer) ready

--- work_2/src/App.js
+++ work_2/src/App.js
@@ -1,38 +1,13 @@
-import './App.css';
+import React from 'react';
+import Timer from './timer';
 
-function MyButton() {
+const App = () => {
   return (
-    <button>
-      I'm a button
-    </button>
-  );
-}
-
-
-
-function App() {
-  <body>
     <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
+      <h1>Таймер</h1>
+      <Timer />
     </div>
-    
-    <input
-        type="number"
-        class="form-control mb-2"
-        placeholder="Введите 1 число"
-        id="input1"
-        value="10"
-      />
-      <input
-        type="number"
-        class="form-control mb-2"
-        placeholder="Введите 2 число"
-        id="input2"
-        value="5"
-      />
-  </body>
-  
-}
+  );
+};
 
 export default App;
